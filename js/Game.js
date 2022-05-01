a644c88ec3d8cd019ea6ce8ada2e6ac47a3175e7
MissIvonneM <90948670+MissIvonneM@users.noreply.github.com>
2022-05-01 17:40:47 -0500

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,54 +1,57 @@
+//Código Inicial
 class Game {
-  constructor() {}
-// Observemos la estructura y los comandos que tenemos en nuestra class Game
-  getState() {
+    constructor() {}
+      // Observemos la estructura y los comandos que tenemos en nuestra class Game
+   getState() {
+  
     //1) .ref pasa la ubicación del campo gameState
     //2) .on() lee el valor del campo 'gameState' y lo guarda en la variable global gameState.
     var gameStateRef = database.ref("gameState");
-    //3) Vinculamos  la función para leer el gameState con sólo getState()
-    gameStateRef.on("value", function(data) {
-      gameState = data.val();
-    });
-    //4) Vamos a sketch para ver donde llamamos el estado del juego
-  }
-
-  //Aa ST) Crea método para actualizar el gameState()
-  update(state) {
-    database.ref("/").update({
-      gameState: state
-    });
-  }
-
-
-  start() {
-    
-
   
-
+      //3) Vinculamos  la función para leer el gameState con sólo getState()
+      gameStateRef.on("value", function(data) {
+      gameState = data.val();
+      });
+    }
+  
+    update(state) {
+      database.ref("/").update({
+        gameState: state
+      });
+    }
+     //4) Vamos a sketch para ver donde llamamos el estado del juego
+   
+  
+  
+    start() {
+      //Crea un objeto para de la class Player y uno de la class Form
+      
+  
+      //6b) Ahora vamos a Player a revisar el Constructor
+    
       //Creamos los autos 1 y 2, les agregamos la imágen y los escalamos
+      
     
 
 
-      //Se agregan al arreglo cars[]
       cars = [car1, car2];
-  }
-
-
-  handleElements() {
-    form.hide();
-    form.titleImg.position(40, 50);
-    form.titleImg.class("gameTitleAfterEffect");
-  }
-//Ba ST) Crea método play que mandamos a llamas en sketch
-  play() {
-    this.handleElements();
-
-    Player.getPlayersInfo();
-
-    if (allPlayers !== undefined) {
-      image(track, 0, -height * 5, width, height * 6);
-
-      drawSprites();
+    }
+  
+  
+   handleElements() {
+     form.hide();
+     form.titleImg.position(40, 50);
+     form.titleImg.class("gameTitleAfterEffect");
+   }
+  
+   play() {
+     //Ba ST) Crea método play que mandamos a llamas en sketch
+     this.handleElements();
+     Player.getPlayersInfo();
+     if (allPlayers !== undefined) {
+       image(track, 0, -height * 5, width, height * 6);
+       drawSprites();
+      }
     }
   }
-}
+  
--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 //Código de Inicio PRO C37 V2 Estados del Juego e información del Jugador
 var canvas;
-var backgroundImage;
+var backgroundImage, bgImg, car1_img, car2_img, track;;
 var database, gameState;
 var form, player, playerCount;
-var allPlayers;
+var allPlayers,car1, car2;
+var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
+   
+  //Carga Las imágenes de los autos y la pista (recuerda agregar las variables globales correspondientes)
+
+
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   database = firebase.database();
   game = new Game();
-
   
 //4a) Aquí llamamos la función getState para leer el estado del juego ANTES de mostrar el formulario
 //4b) Primero llamamos la función getState() y después start()
@@ -28,7 +32,7 @@ function draw() {
  //15 TA) Se el numero de jugadores es 2, el gameState se convierte en 1;
   if (playerCount === 2) {
     game.update(1);
-  }
+    }
 
  //Si el gameState se convierte en 1 entonces se llama al módulo play de gameState
  //e Inicia el juego.
